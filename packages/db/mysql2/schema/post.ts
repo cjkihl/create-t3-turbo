b596ee9c11292c81beb16ac983541e710a999743
cjkihl <carljohan.kihl@gmail.com>
2023-07-11 14:42:46 +0800

    bugfixes

--- packages/db/mysql2/schema/post.ts
+++ packages/db/mysql2/schema/post.ts
@@ -2,6 +2,6 @@ import { mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
 
 export const posts = mysqlTable("posts", {
   id: serial("id").primaryKey(),
-  title: varchar("name", { length: 255 }).notNull(),
-  content: varchar("name", { length: 255 }).notNull(),
+  title: varchar("title", { length: 255 }).notNull(),
+  content: varchar("content", { length: 255 }).notNull(),
 });
--- packages/db/pg/schema/post.ts
+++ packages/db/pg/schema/post.ts
@@ -2,6 +2,6 @@ import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const posts = pgTable("posts", {
   id: serial("id").primaryKey(),
-  title: varchar("name", { length: 255 }).notNull(),
-  content: varchar("name", { length: 255 }).notNull(),
+  title: varchar("title", { length: 255 }).notNull(),
+  content: varchar("content", { length: 255 }).notNull(),
 });
